afa75096a7d10a3e6a3578d70ad4c9e661c5a251
shashankkumarP <nawalerason@gmail.com>
2022-10-09 23:27:01 +0530

    final

--- index.js
+++ index.js
@@ -11,9 +11,17 @@ dotenv.config();
 const port = Number(process.env.PORT);
 const url  = process.env.MONGODB_URL;
 const app = express()
+app.use(
+    cors({
+      origin: "*",
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+    })
+  );
 app.use(express.json())
 app.use(express.urlencoded())
-app.use(cors())
+
 
 
 
@@ -31,6 +39,9 @@ const User = new mongoose.model("userinfo", userSchema)
 // })
 
 // Routes
+app.get("/",(req,res)=>{
+    res.send("connected");
+})
 app.post("/login", (req, res)=> {
 
     const { email, password} = req.body
